cff4ff35fbbbe07027247a7db0cdbea1193f7bf5
DagnyJay <59747157+DagnyJay@users.noreply.github.com>
2020-04-11 14:57:33 -0600

    connect to DB

--- server.js
+++ server.js
@@ -2,18 +2,16 @@ const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
-const port = process.env.PORT || 3001;
 
 const app = express();
+
+require('dotenv').config();
+require('./config/database');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
-
-app.listen(port, function() {
-    console.log(`Express app running on port ${port}`)
-  });
 
 // Put API routes here, before the "catch all" route
 
@@ -21,4 +19,10 @@ app.listen(port, function() {
 // Since this route is a "catch all" that matches every get request, be sure to mount API or other routes before it!
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, function() {
+    console.log(`Express app running on port ${port}`)
 });
